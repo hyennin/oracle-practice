e44ad1b8ef9a2e112b2757fb5db5f69c25d3dcf8
hyennin <chm050202@gmail.com>
2023-04-07 15:10:44 +0900

    날짜 실습

--- 20230404.sql
+++ 20230404.sql
@@ -40,7 +40,7 @@ FLOOR(MOD((SYSDATE - hire_date), 7)) 일
 FROM employees WHERE department_id = 30 ORDER BY 3 DESC;
 
 SELECT department_id, hire_date, FLOOR(SYSDATE - hire_date) 결과, 
-FLOOR((SYSDATE - hire_date) / 30) 달,
+FLOOR((SYSDATE - hire_date) / 30) 월,
 FLOOR(MOD(SYSDATE - hire_date, 30) / 7) 주, 
 FLOOR(MOD(MOD(SYSDATE - hire_date, 30), 7)) 일
 FROM employees 
@@ -49,9 +49,9 @@ ORDER BY 3 DESC;
 
 SELECT department_id, hire_date, FLOOR(SYSDATE - hire_date) 결과, 
 FLOOR((SYSDATE - hire_date) / 365) 년,
-FLOOR(MOD(SYSDATE - hire_date, 365) / 30) 달,
+FLOOR(MOD(SYSDATE - hire_date, 365) / 30) 월,
 FLOOR(MOD(MOD(SYSDATE - hire_date, 365), 30)/ 7) 주,
 FLOOR(MOD(MOD(MOD(SYSDATE - hire_date, 365), 30), 7)) 일
 FROM employees 
-WHERE department_id = 30 
+WHERE department_id = 30
 ORDER BY 3 DESC;
