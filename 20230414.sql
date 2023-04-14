3ca38d7d4f96a87bff387acd0ae29227d5379efb
hyennin <chm050202@gmail.com>
2023-04-14 15:13:02 +0900

    일반 함수 실습

--- 20230414.sql
+++ 20230414.sql
@@ -34,3 +34,5 @@ WHERE department_id IN(30, 80);
 SELECT department_id, first_name, salary, commission_pct, salary * (1 + NVL(commission_pct, 0)) "금월급여" 
 FROM employees 
 WHERE department_id IN(30, 80);
+
+SELECT department_id, first_name, manager_id, NVL2(manager_id, manager_id || '담당', '상위자') 담당매니저 FROM employees WHERE department_id IN(20, 90);
